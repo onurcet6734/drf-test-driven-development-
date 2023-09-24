1f1d4e2a84a9ff6ff6bacd1be602fc1cd641a71c
TechWorm <TechWorm@DESKTOP-U9GLI9A>
2023-09-24 15:50:11 +0300

    finished serialization operations

--- order/api/serializers.py
+++ order/api/serializers.py
@@ -19,7 +19,7 @@ class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
-        fields = ['customer', 'menuitem', 'total_price', 'order_date']
+        fields = "__all__"
 
     def create(self, validated_data):
         """
@@ -38,21 +38,50 @@ class OrderSerializer(serializers.ModelSerializer):
         order.menuitem.set(menuitems) #set was used because of the many-to-many relationship
 
         return order
+    
+    # def create_or_update_menuitems(self, menuitems):
+    #     menuitem_ids = []
+    #     for menuitem in menuitems:
+    #         menuitem_instance, created = MenuItem.objects.update_or_create(pk=menuitem.get('id'), defaults=menuitem)
+    #         menuitem_ids.append(menuitem_instance.pk)
+    #     return menuitem_ids
 
     def update(self, instance, validated_data):
         """
         Update and return an existing Order instance, given the validated data.
         """
-        instance.customer = validated_data.get('customer', instance.customer)
+        customer_data = validated_data.pop('customer', [])
+        if customer_data:
+            customer, created = Customer.objects.get_or_create(**customer_data)
+            instance.customer = customer
+
         instance.total_price = validated_data.get('total_price', instance.total_price)
         instance.order_date = validated_data.get('order_date', instance.order_date)
 
-        # Update the items in the order
+        # Menuitem güncellemesi
         if 'menuitem' in validated_data:
-            instance.menuitem.clear()
-            for item_data in validated_data['menuitem']:
-                item = MenuItem.objects.get(id=item_data['id'])
-                instance.menuitem.add(item)
+            menuitem_data = validated_data.pop('menuitem', [])
+
+            # Mevcut menuitem'ları alın
+            current_menuitems = list(instance.menuitem.all())
+
+            # Güncellenecek menuitem'ları toplayın
+            updated_menuitems = []
+
+            for item_data in menuitem_data:
+                menuitem_id = item_data.get('id')
+
+                # Eğer menuitem_id verilerde mevcut değilse veya mevcut menuitem'ların içinde bu ID yoksa, yeni menuitem yaratın
+                if not menuitem_id or menuitem_id not in [item.id for item in current_menuitems]:
+                    menuitem, created = MenuItem.objects.get_or_create(**item_data)
+                else:
+                    # Eğer menuitem_id mevcut ise, bu ID'ye sahip menuitem'ı kullanın
+                    menuitem = MenuItem.objects.get(id=menuitem_id)
+
+                updated_menuitems.append(menuitem)
+
+            # Menuitem'ları güncelleyin
+            instance.menuitem.set(updated_menuitems)
 
         instance.save()
         return instance
