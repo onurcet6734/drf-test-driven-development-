b1d514ad38a6a78d7f325c561a4bcf398b2eb3cc
TechWorm <TechWorm@DESKTOP-U9GLI9A>
2023-09-24 15:52:53 +0300

    finished serialization operations

--- order/api/serializers.py
+++ order/api/serializers.py
@@ -39,15 +39,9 @@ class OrderSerializer(serializers.ModelSerializer):
 
         return order
     
-    # def create_or_update_menuitems(self, menuitems):
-    #     menuitem_ids = []
-    #     for menuitem in menuitems:
-    #         menuitem_instance, created = MenuItem.objects.update_or_create(pk=menuitem.get('id'), defaults=menuitem)
-    #         menuitem_ids.append(menuitem_instance.pk)
-    #     return menuitem_ids
 
     def update(self, instance, validated_data):
-        """
+        """ 
         Update and return an existing Order instance, given the validated data.
         """
         customer_data = validated_data.pop('customer', [])
@@ -58,29 +52,29 @@ class OrderSerializer(serializers.ModelSerializer):
         instance.total_price = validated_data.get('total_price', instance.total_price)
         instance.order_date = validated_data.get('order_date', instance.order_date)
 
-        # Menuitem güncellemesi
+        # Updating manytomany menuitems
         if 'menuitem' in validated_data:
             menuitem_data = validated_data.pop('menuitem', [])
 
-            # Mevcut menuitem'ları alın
+            # getting available menuitems
             current_menuitems = list(instance.menuitem.all())
 
-            # Güncellenecek menuitem'ları toplayın
+            # Colleting all of menuitems
             updated_menuitems = []
 
             for item_data in menuitem_data:
                 menuitem_id = item_data.get('id')
 
-                # Eğer menuitem_id verilerde mevcut değilse veya mevcut menuitem'ların içinde bu ID yoksa, yeni menuitem yaratın
+                # If menuitem_id is not present in the data or if existing menuitems do not have this ID, create a new menuitem
                 if not menuitem_id or menuitem_id not in [item.id for item in current_menuitems]:
                     menuitem, created = MenuItem.objects.get_or_create(**item_data)
                 else:
-                    # Eğer menuitem_id mevcut ise, bu ID'ye sahip menuitem'ı kullanın
+                    # If menuitem_id is available, use the menuitem with this ID
                     menuitem = MenuItem.objects.get(id=menuitem_id)
 
                 updated_menuitems.append(menuitem)
 
-            # Menuitem'ları güncelleyin
+            # Updating menuitems
             instance.menuitem.set(updated_menuitems)
 
         instance.save()
