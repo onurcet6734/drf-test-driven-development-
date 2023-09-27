29a195538980612dea244e2aa9c011a628616712
TechWorm <TechWorm@DESKTOP-U9GLI9A>
2023-09-27 15:38:58 +0300

    comment codes will be fixed

--- customer/tests.py
+++ customer/tests.py
@@ -34,19 +34,18 @@ class CustomerTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
-    # def test_list_customers(self):
-    #     response = self.client.post('/customer/list-create', self.customer_data, format='json')
-    #     response = self.client.get('/customer/list-create', format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertEqual(len(response.data), 1)
+    def test_list_customers(self):
+        response = self.client.post(self.url_listCreate, customer_data = {'name': 'Admin',"user": self.user.id}, format='json')
+        response = self.client.get(self.url_listCreate, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        
 
-    # def test_update_customer(self):
-    #     response = self.client.post(self.url_listCreate, self.customer_data, format='json')
-    #     customer_id = response.data['id']
-    #     updated_customer_data = {'name': 'Updated Customer'}
-    #     response = self.client.put(f'/customer/update-delete/{customer_id}', updated_customer_data, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertEqual(Customer.objects.get().name, 'Updated Customer')
+    def test_update_customer(self):
+          customer_data = {'name': 'Admin3'}
+          updateDelete_url = reverse("customer:customer-detail", kwargs={"pk": Customer.objects.create(name="Admin", user=self.user).pk})
+          response = self.client.put(updateDelete_url, customer_data, format='json')
+          self.assertEqual(200, response.status_code)
 
     # def test_delete_customer(self):
     #     response = self.client.post('/customer/list-create', self.customer_data, format='json')
